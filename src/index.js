03b21935cf68ba57dcc51bfca43c084f2414f15d
Danas <Strumilo@users.noreply.github.com>
2020-01-27 21:00:39 +0200

    first

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './assets/styles/app.scss';
+import './index.hbs'
+
+import './assets/images/menu-24px.svg'
+import './assets/images/more_horiz-24px.svg'
+import './assets/images/notifications-24px.svg'
+import './assets/images/offline_bolt-24px.svg'
+import './assets/images/portrait1.jpg'
+import './assets/images/search-24px.svg'
+import './assets/images/all_inclusive-24px.svg'
+import './assets/images/ballot-24px.svg'
+import './assets/images/bar_chart-24px.svg'
+import './assets/images/build-24px.svg'
+import './assets/images/card_giftcard-24px.svg'
+import './assets/images/amazon.png'
+import './assets/images/green-circle.png'
+import './assets/images/orange-circle.png'
+import './assets/images/violet-circle.png'
+
+
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'assets/images',
+          publicPath: './images'
+        },
+      },
+      { test: /\.hbs$/, loader: "handlebars-loader" }
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'assets/styles/app.css',
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        handlebarsLoader: {}
+      }
+    }),
+    new HtmlWebpackPlugin({
+      title: 'index',
+      filename: 'index.html',
+      template: './src/index.hbs'
+    }),
+  ]
+};
